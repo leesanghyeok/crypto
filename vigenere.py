02f8d90054818c7176e36c04c523732571fd5e1f
leesanghyeok <1004xlsrn@gmail.com>
2015-01-05 10:52:32 +0000

    complete vigenere

---
+++ vigenere.py
@@ -0,0 +1,33 @@
+import string
+lower = string.ascii_lowercase
+
+def equalstr(st,length):
+  newstr = ""
+  j =0
+  for i in range(length):
+    newstr = newstr + st[j]
+    j=j+1
+    if j == len(st):
+      j = 0
+  return newstr
+
+def encrypt(plain,key):
+  nkey = equalstr(key,len(plain))
+  cipher = ""
+  for i in range(len(plain)):
+    cipher = cipher + lower[(lower.index(plain[i])+lower.index(nkey[i]))%len(lower)]
+  return cipher
+
+def decrypt(cipher,key):
+  nkey = equalstr(key,len(cipher))
+  plain = ""
+  for i in range(len(cipher)):
+    plain = plain + lower[(lower.index(cipher[i])-lower.index(nkey[i]))%len(lower)]
+  return plain 
+
+
+plain = "helloworld"
+key = "clang"
+
+print encrypt(plain,key)
+print decrypt(encrypt(plain,key),key)
