23b926b8a1d442425b54d66203a1807f352c1c7b
leesanghyeok <1004xlsrn@gmail.com>
2015-03-16 10:21:39 +0000

    make will create the combined python script and make the file name as argv[1]

---
+++ make.py
@@ -0,0 +1,22 @@
+import os
+import sys
+#show the file list in current directory
+curdir = os.getcwd()
+pathlist = [f for f in os.listdir(curdir) if os.path.isfile(os.path.join(curdir,f)) and os.path.splitext(f)[1]=='.py']
+#read and show the file
+textlist = []
+for i in pathlist:
+    f = open(os.path.join(curdir,i),'r')
+    textlist.append(f.read())
+    f.close()
+
+#put the texts together
+output = "".join(textlist)
+if len(sys.argv)==2:
+    filename = sys.argv[1]
+else:
+    filename = "default.py"
+
+f = open(os.path.join(curdir,filename),'w')
+f.write(output)
+f.close()
